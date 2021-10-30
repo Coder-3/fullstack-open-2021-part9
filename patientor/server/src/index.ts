5b8993ff17fb8381499cc01e45da2b7d8a3cbac9
Cedric Quenette <cedricquenette@outlook.com>
2021-10-31 09:35:38 +1100

    9.10: Patientor backend, step3

--- patientor/server/src/index.ts
+++ patientor/server/src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+
+import diagnosesRouter from './routes/diagnoses';
+
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
